cd0c61303006e42dc3720443ff0bf8bfb9ebdd85
Alexandrea <lexhom24@gmail.com>
2022-06-09 13:11:54 -0400

    added tictactoe

--- js/app.js
+++ js/app.js
@@ -1,6 +1,24 @@
-
+import { projectData } from "./project-data.js"
 
 const cardContainer = document.getElementById("card-container")
 
 
-// console.log(cardContainer);
+let projectDataMarkup = projectData.map(project =>
+  `
+  <div class="card" style="width: 18rem;">
+  <img src="${project.image}" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">${project.title}</h5>
+      <p class="card-text">${project.description}</p>
+      <div>
+        <a href="${project.github}" class="btn btn-primary">Github</a>
+        <a href="${project.deployment}" class="btn btn-secondary">Deployment</a>
+      </div>
+    </div>
+  </div> 
+  
+  `
+  ).join('')
+
+  cardContainer.innerHTML = projectDataMarkup
+  console.log(projectDataMarkup);
